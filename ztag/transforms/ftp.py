902c885fa7cd0d7c46a9bcbfe4814e8182c4a81c
Drew Springall <aaspring>
2015-11-04 17:51:27 -0500

    Add transform for FTPS ztag output

--- ztag/transforms/ftp.py
+++ ztag/transforms/ftp.py
@@ -1,6 +1,7 @@
 from ztag.transform import ZGrabTransform, ZMapTransformOutput
 from ztag import protocols, errors
 from ztag.transform import Transformable
+import https
 
 class FTPTransform(ZGrabTransform):
 
@@ -27,6 +28,43 @@ class FTPTransform(ZGrabTransform):
 
         if len(out) == 0:
             raise errors.IgnoreObject("Empty output dict")
+        out['ip_address'] = obj['ip']
+        out['timestamp'] = obj['timestamp']
+        zout.transformed = out
+        return zout
+
+class FTPSTransform(ZGrabTransform):
+    name = "ftp/ftps"
+    port = 21
+    protocol = protocols.FTP
+    subprotocol = protocols.FTP.TLS
+
+    def __init__(self, *args, **kwargs):
+        super(FTPSTransform, self).__init__(*args, **kwargs)
+
+    def _transform_object(self, obj):
+        ftp_banner = obj
+        ftp = Transformable(obj)
+        zout = ZMapTransformOutput()
+        error = ftp['error'].resolve()
+        if error is not None:
+            raise errors.IgnoreObject("Error")
+
+        out = dict()
+        banner = ftp['data']['ftp']['banner'].resolve()
+
+        if banner is not None:
+            out['banner'] = self.clean_banner(banner)
+
+        if len(out) == 0:
+            raise errors.IgnoreObject("Empty output dict")
+
+        if "tls" in obj['data']:
+            tls = obj['data']['tls']
+            peerCertData, certificates = https.HTTPSTransform.make_tls_obj(tls)
+            out["tls"] = peerCertData
+            zout.certificates = certificates
+
         out['ip_address'] = obj['ip']
         out['timestamp'] = obj['timestamp']
         zout.transformed = out
